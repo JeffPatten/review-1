b5a00635e9f2ae531b6532773f73761da3dbe827
JeffPatten <jeffspatten@gmail.com>
2024-02-27 14:36:44 -0600

    array reviewed

---
+++ week1-review.js
@@ -0,0 +1,13 @@
+//Arrays - collection of data. Usually, a collection of like data, ex, people, favorite foods, prices
+
+     let varName = ['hello', 12, true, [], {}, undefined, null] //can contain any data type
+// index/position      0     1     2   3   4    5          6
+// length              1     2    3    4   5    6          7
+
+//accessing values - like an address street[house number]
+     varName[0]  //  returns 'hello'
+     varName[2]  // returns true
+
+// properties - information about the array, methods - functions that we can run on the array
+     varName.length // property - 7
+     varName.pop() .push('howdy') // drops the last item in the array / add's an item to the end of the array
