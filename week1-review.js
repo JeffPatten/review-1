dcd0581f6206b743011b384d98f055f3e7babaf1
JeffPatten <jeffspatten@gmail.com>
2024-03-04 15:55:15 -0600

    data types and how to access complex data types

--- week1-review.js
+++ week1-review.js
@@ -1,39 +1,84 @@
-                                                               // Variables
+/*----  Data types or values  ----*/
+
+// Primitive or simple data types = string, number, boolean(true or false), undefined, null
+// arrays, objects
+
+let string = "some string of text";   //anything you put between single quotes (''), double quotes (""), or template strings (``)
+let num = 12;
+let boolean = true; // or false
+let isDefined; // this is undefined
+let nothing = null;
+let arr = ["something", 3, false, [], {}]; //collection of data that correlate. ie favorite foods
+let obj = {
+  //collection of data that correlates but allows us to name properties/keys
+  name: "jeff",
+  favColor: "blue",
+  favNum: 18,
+};
+
+/*----  Variables  ----*/
+
 //declarations/declaring variables
-let aVariable      // let's you reassign a value
-const aVariable2 = 'a value'  // constant value - must give it a value
+let username = ""; // The 'let' keyword let's you reassign a value
+const aVariable2 = "a value"; // constant value - must give it a value, and it will always stay that value
+const isSkyBlue = true; // const for anything you want to alsway stay the same
 
 //reassign a value
 
-aVariable = 10
+username = "jPatten";
 // aVariable2 = 12    const variables cannot be reassigned a value
 
-// Variables are like a file cabinet drawer that you have labeled. When you want to use what is in that drawer you use a label. 
+// Variables are like a file cabinet drawer that you have labeled. When you want to use what is in that drawer you use a label.
+if (username) {                                 // if('jPatten') {
+  console.log(`the username is ${username}`);   // console.log(`the username is jPatten`)
+}                                               // }
 
+/*----  Arrays - collection of data. Usually, a collection of like data, ex, people, favorite foods, prices  ----*/
 
-                                                              //Data types or values
+    let arr2 = ["hello", 12, true, [], {}, undefined, null];   //can contain any data type. An ordered list of data
+// index/position  0     1    2    3   4    5          6      
+// length          1     2    3    4   5    6          7
 
-// string, number, boolean(true or false), undefined, null
-// arrays, objects
+//accessing values - you are accessing a value by referencing the array and then the index in square brackets. like an address street[house number].  varName[0] reads as the array at index/position 0, or 1, or 2, etc
+arr2[0]; //  returns 'hello'               
+arr2[2]; // returns true
 
-
-                           //Arrays - collection of data. Usually, a collection of like data, ex, people, favorite foods, prices
-
-     let varName = ['hello', 12, true, [], {}, undefined, null] //can contain any data type. An ordered list of data
-// index/position      0     1     2   3   4    5          6
-// length              1     2    3    4   5    6          7
-
-//accessing values - like an address street[house number]
-     varName[0]  //  returns 'hello'
-     varName[2]  // returns true
+// (varName.length - 1) is the last index/position in the array. length is 7, position/index is 6
 
 // properties - information about the array, methods - functions that we can run on the array
-     varName.length // property - 7
-     varName.pop() .push('howdy') // drops the last item in the array / add's an item to the end of the array
 
-                                                     // Objects - collection of data, with key value pairs. The keys describe what the values are. (ex firstName: 'Jeff')
-     let person = {
-          firstName: 'Jeff',           //left side is key, right side is value
-          lastName: 'Patten',
-          favColor: 'blue?'
-     }
+arr2.length; // this is a property - 7
+arr2.pop().push("howdy"); // drops the last item in the array / add's an item to the end of the array. These are called methods/functions
+
+
+//for loops - this loops through an array, while loop, .map(), .filter(), .forEach(). The whole purpose of loops is to get each value of an array and do some code with it.
+
+for (let index = 0; index < arr2.length; index++) {
+     console.log(arr2[index])
+}
+
+/*----  Objects - collection of data, with key value pairs. The keys describe what the values are. (ex firstName: 'Jeff')  ----*/
+
+let person = {                                               // The only reason each key/value is in a separate line is to make it easier to read.
+  firstName: "Jeff",    // left side is key(property), right side is value
+  lastName: "Patten",
+  favColor: "blue"
+};
+
+let person2 = {
+     firstName: 'Arianna',
+     lastName: 'Contardo',
+     favoriteColor: 'pink'
+}
+
+// accessing values
+
+// dot notation - object name dot key name = obj.key  !!!!MOST USED!!!!
+person.favColor // this returns 'blue'
+person2.favoriteColor //this returns 'pink'
+
+//bracket notation - use a string for the key name or a variable.  !!!MOSTLY USED in specific loops - not often used!!!
+person['favColor'] // with quotes, you are using a key name.           returns 'blue'
+
+let color = 'favoriteColor' 
+person2[color] // => person2['favoriteColor'] // without quotes, you are using a variable name.   returns 'pink
