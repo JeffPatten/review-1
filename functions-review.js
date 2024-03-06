bc20dcdf916e7ca20006b1365bc93591bc249f73
JeffPatten <jeffspatten@gmail.com>
2024-03-06 16:32:13 -0600

    reviewed conditionals and functions

---
+++ functions-review.js
@@ -0,0 +1,77 @@
+/*          --------------------------------------- FUNCTIONS ---------------------------------------------   */
+
+                                        // function declaration
+
+function funcDec () {
+
+}
+
+                                        // function expression
+
+const funcExpression = function () {
+
+}
+
+                                        // arrow functions
+
+const funcExpressArrow = () => {}
+const funcExpressArrow2 = param1 => {}       // if you have one and only 1 parameter, you don't have to use the parentheses.
+const inlineArrowFunc = () => true           // if you are only returning something, you can leave off the curly brackets and the return keyword.
+
+                                        // The anatomy of a function
+
+// function functionName (input or parameters or take in) {
+//     code that is run when the function is called/invoked/used
+//     return output
+// }
+
+// Calling a function
+
+// functionName(arguments)
+
+
+
+// Problem 1
+
+// Write a function called damageAndDebuff that
+// can take in `healthTotal`,`baseDamage`,
+// and `healthDebuff` as parameters.
+
+// Inside the function, subtract
+// the value of the base damage from the health total, 
+// and update the total to that value. 
+// Calculate the healthDebuff and 
+// subtract it from the total. 
+// Return the new healthTotal.  
+
+// Note: the numbers passed in for `healthDebuff` will be
+// decimals, for example: .15 for a 15% health debuff.
+
+
+function damageAndDebuff (healthTotal, baseDamage, healthDebuff) {    // It is like you are describing how the code will run
+    
+    healthTotal = healthTotal - baseDamage
+    //  healthTotal * (1 - healthDebuff)                           // 90 (1 - .15)  => 90 (.85) => 76.5
+    healthTotal = healthTotal - (healthDebuff * healthTotal)       // 90 - (.15 * 90) => 90 - 13.5 => 76.5
+    return healthTotal
+}
+
+damageAndDebuff(100, 10, .15)  // let healthTotal = 100, let baseDamage = 10, let healthDebuff = .15  Actually running the code with these values
+
+
+// PRACTICE CODE 
+// const character = {
+//     name: 'Gimli',
+//     health: 100
+// }
+
+// function damageAndDebuff (char, baseDamage) {
+//     // console.log(char.health - baseDamage)
+//     // console.log(char.health)
+//     return char.health - baseDamage       // return 90
+// }
+
+// // damageAndDebuff(character, 10, .15) //healthTotal = 100, baseDamage = 10, healthDebuff = .15
+// let newHealth = damageAndDebuff(character, 10, .15)
+// console.log(newHealth)
+
--- week1-review.js
+++ week1-review.js
@@ -3,7 +3,7 @@
 // Primitive or simple data types = string, number, boolean(true or false), undefined, null
 // arrays, objects
 
-let string = "some string of text";   //anything you put between single quotes (''), double quotes (""), or template strings (``)
+let string = "some string of text"; //anything you put between single quotes (''), double quotes (""), or template strings (``)
 let num = 12;
 let boolean = true; // or false
 let isDefined; // this is undefined
@@ -29,18 +29,19 @@ username = "jPatten";
 // aVariable2 = 12    const variables cannot be reassigned a value
 
 // Variables are like a file cabinet drawer that you have labeled. When you want to use what is in that drawer you use a label.
-if (username) {                                 // if('jPatten') {
-  console.log(`the username is ${username}`);   // console.log(`the username is jPatten`)
-}                                               // }
+if (username) {
+  // if('jPatten') {
+  console.log(`the username is ${username}`); // console.log(`the username is jPatten`)
+} // }
 
 /*----  Arrays - collection of data. Usually, a collection of like data, ex, people, favorite foods, prices  ----*/
 
-    let arr2 = ["hello", 12, true, [], {}, undefined, null];   //can contain any data type. An ordered list of data
-// index/position  0     1    2    3   4    5          6      
+let arr2 = ["hello", 12, true, [], {}, undefined, null]; //can contain any data type. An ordered list of data
+// index/position  0     1    2    3   4    5          6
 // length          1     2    3    4   5    6          7
 
 //accessing values - you are accessing a value by referencing the array and then the index in square brackets. like an address street[house number].  varName[0] reads as the array at index/position 0, or 1, or 2, etc
-arr2[0]; //  returns 'hello'               
+arr2[0]; //  returns 'hello'
 arr2[2]; // returns true
 
 // (varName.length - 1) is the last index/position in the array. length is 7, position/index is 6
@@ -48,37 +49,71 @@ arr2[2]; // returns true
 // properties - information about the array, methods - functions that we can run on the array
 
 arr2.length; // this is a property - 7
-arr2.pop().push("howdy"); // drops the last item in the array / add's an item to the end of the array. These are called methods/functions
-
+// arr2.pop().push("howdy"); // drops the last item in the array / add's an item to the end of the array. These are called methods/functions
 
 //for loops - this loops through an array, while loop, .map(), .filter(), .forEach(). The whole purpose of loops is to get each value of an array and do some code with it.
 
 for (let index = 0; index < arr2.length; index++) {
-     console.log(arr2[index])
+  console.log(arr2[index]);
 }
 
 /*----  Objects - collection of data, with key value pairs. The keys describe what the values are. (ex firstName: 'Jeff')  ----*/
 
-let person = {                                               // The only reason each key/value is in a separate line is to make it easier to read.
-  firstName: "Jeff",    // left side is key(property), right side is value
+let person = {
+  // The only reason each key/value is in a separate line is to make it easier to read.
+  firstName: "Jeff", // left side is key(property), right side is value
   lastName: "Patten",
-  favColor: "blue"
+  favColor: "blue",
 };
 
 let person2 = {
-     firstName: 'Arianna',
-     lastName: 'Contardo',
-     favoriteColor: 'pink'
-}
+  firstName: "Arianna",
+  lastName: "Contardo",
+  favoriteColor: "pink",
+};
 
 // accessing values
 
 // dot notation - object name dot key name = obj.key  !!!!MOST USED!!!!
-person.favColor // this returns 'blue'
-person2.favoriteColor //this returns 'pink'
+person.favColor; // this returns 'blue'
+person2.favoriteColor; //this returns 'pink'
 
 //bracket notation - use a string for the key name or a variable.  !!!MOSTLY USED in specific loops - not often used!!!
-person['favColor'] // with quotes, you are using a key name.           returns 'blue'
+person["favColor"]; // with quotes, you are using a key name.           returns 'blue'
 
-let color = 'favoriteColor' 
-person2[color] // => person2['favoriteColor'] // without quotes, you are using a variable name.   returns 'pink
+let color = "favoriteColor";
+person2[color]; // => person2['favoriteColor'] // without quotes, you are using a variable name.   returns 'pink
+
+/* -------------------------------- CONDITIONALS ------------------------------- */
+
+// if (a condition - truthy/falsy) {                            // The 'if' keyword lets javascript know to expect certain syntax (grammar). ie there should be parenthesis and then curly brackets
+      //run some code
+// } else if (another condition ) {                             // with multiple conditions, javascript will find the first true condition and run it's code, then quit the rest of the if/else statements
+      //run some code
+// } else {
+      //run some code
+// }
+
+// falsy values = an empty string (''), 0, false, null, undefined
+
+let isRaining = false;
+
+if (isRaining){                      //whoAmI is a string that has a value. It evaluates as true or as a truthy value. 
+  console.log(`It's so rainy!`)
+} else {
+  console.log(`It's so sunny!!!`)
+}
+
+let whoAmI = `I don't know.`;
+console.log(whoAmI)
+
+if (whoAmI === `I'm Jeff`) {
+  console.log('Party Time')
+} else if ( whoAmI === `I don't know.`) {
+  console.log(`It's time to find yourself.`)
+  whoAmI = `I'm Jeff`
+} else if (whoAmI === `I don't know.`) {                 //won't run because the previous else-if statement was true
+  console.log('Will this run?')
+}
+
+console.log(whoAmI)
